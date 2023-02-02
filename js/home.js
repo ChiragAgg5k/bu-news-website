65e81091084bd9367d182fd212955976ebf0175d
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-02-02 22:37:26 +0530

    added dark mode toggle

--- js/home.js
+++ js/home.js
@@ -1,6 +1,9 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+const toggleButton = document.getElementById('toggle-icon');
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("topbar").style.top = "0";
@@ -13,3 +16,24 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// toggle dark mode
+function toggleDarkMode() {
+  var element = document.body;
+  var a = document.querySelectorAll("a");
+  var nav = document.getElementById("mainnav");
+  var topBorder = document.getElementById("topbar");
+  
+  element.classList.toggle("dark-mode");
+  nav.classList.toggle("nav-dark-mode");
+  topBorder.classList.toggle("topborder-dark-mode");
+
+  for (var i = 0; i < a.length; i++) {
+    a[i].classList.toggle("a-dark-mode");
+  }
+}
+
+toggleButton.addEventListener('click',e => {
+  toggleDarkMode();
+  toggleButton.classList.toggle('fa-sun');
+});
