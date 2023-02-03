93e382ca794cdbb571ddfc551463a06bac5f38cf
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-02-03 19:51:29 +0530

    news block

--- js/home.js
+++ js/home.js
@@ -1,1 +1,6 @@
+const home_button = document.getElementById('home-button');
+const home_page = document.querySelector('.home-page');
 
+function hideAll(){
+    home_page.classList.add('hidden');
+}
