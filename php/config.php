f96df5149abcb77221414b617a07dd9fce27bdc6
Aman Boora <aman.chaudharyboora@gmail.com>
2023-02-04 21:22:41 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,21 +1,91 @@
 <?php
+session_start();
 
-$name = $_POST['name'];
-$email = $_POST['eame'];
-$password = $_POST['password'];
-$enroll = $_POST['enroll'];
+// initializing variables
+$name = "";
+$email    = "";
+$enroll ="";
+$errors = array(); 
 
-//Database connection
-$conn = mysqli_connect('localhost,'root','',signup');
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'signup');
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}else{
-    $dat = $conn->prepare("insert into register(name, email, enroll, password) 
-    values(?,?,?,?)");
-    $dat->bind_param("ssss",$name,$email,$enroll,$password);
-    $dat->execute();
-    echo "Registered Successfully...";
-    $dat->close();
-    $conn->close();
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $name = mysqli_real_escape_string($db, $_POST['name']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $enroll = mysqli_real_escape_string($db, $_POST['enroll']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($name)) { array_push($errors, "Name is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($enroll)) { array_push($errors, "Enrollment ID is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE name='$name' OR email='$email' OR enroll='$enroll' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['name'] === $name) {
+      array_push($errors, "Name already exists");
+    }
+
+    if ($user['enroll'] === $enroll) {
+      array_push($errors, "Enrollment ID already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Nmail already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (name, email, enroll, password) 
+  			  VALUES('$name', '$email', '$enroll', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['name'] = $name;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.html');
+  }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($email)) {
+  	array_push($errors, "Email is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['email'] = $email;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.html');
+  	}else {
+  		array_push($errors, "Wrong name/password combination");
+  	}
+  }
+}
+
 ?>
