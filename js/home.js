50047d5c33ae5f17e6a1158750c4ac0d9583be62
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-02-04 21:22:59 +0530

    made many changes

--- js/home.js
+++
@@ -1,6 +0,0 @@
-const home_button = document.getElementById('home-button');
-const home_page = document.querySelector('.home-page');
-
-function hideAll(){
-    home_page.classList.add('hidden');
-}
--- php/config.php
+++ php/config.php
@@ -6,13 +6,12 @@ $password = $_POST['password'];
 $enroll = $_POST['enroll'];
 
 //Database connection
-$conn = mysqli_connect('localhost,'root','',signup');
+$conn = new mysqli('localhost','root','','signup');
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error());
 }else{
-    $dat = $conn->prepare("insert into register(name, email, enroll, password) 
-    values(?,?,?,?)");
+    $dat = $conn->prepare("insert into register(name, email, enroll, password) values(?,?,?,?)");
     $dat->bind_param("ssss",$name,$email,$enroll,$password);
     $dat->execute();
     echo "Registered Successfully...";
