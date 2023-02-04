f454132eec9f77cc3e015570f90aa70cded475d3
Aman Boora <aman.chaudharyboora@gmail.com>
2023-02-04 20:41:30 +0530

    Update config.php
    added connect info with sql

--- php/config.php
+++ php/config.php
@@ -1,14 +1,21 @@
 <?php
 
-$HOST = 'localhost';
-$USER = 'root';
-$PASSWORD = '';
-$DB = 'bu-news-db';
+$name = $_POST['name'];
+$email = $_POST['eame'];
+$password = $_POST['password'];
+$enroll = $_POST['enroll'];
 
-$conn = mysqli_connect($HOST, $USER, $PASSWORD, $DB);
+//Database connection
+$conn = mysqli_connect('localhost,'root','',signup');
 
-if ($conn->connect_error) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}
-
+}else{
+    $dat = $conn->prepare("insert into register(name, email, enroll, password) 
+    values(?,?,?,?)");
+    $dat->bind_param("ssss",$name,$email,$enroll,$password);
+    $dat->execute();
+    echo "Registered Successfully...";
+    $dat->close();
+    $conn->close();
 ?>
