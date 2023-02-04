cff871893c02805476ee330466c76ce76fbdba70
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-02-05 02:03:11 +0530

    added more changes

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 fetch('../html/footer.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
+    let oldelem = document.querySelector("script#replace_with_footer");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
