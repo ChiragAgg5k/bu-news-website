dc8e3d1924a794598e828c2ef94e62138ebb7956
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-07-05 16:48:09 +0530

    fixed prettier-plugin-tailwind

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// .prettierrc.js
+
+module.exports = {
+	arrowParens: 'always',
+	printWidth: 120,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: 'es5',
+	useTabs: true,
+	plugins: [require('prettier-plugin-tailwindcss')],
+}
